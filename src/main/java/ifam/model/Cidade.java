5c7052dc1353420e550438705b9d1b30a42d0341
EscolaDev <escoladevbrasil@gmail.com>
2023-08-26 10:29:56 -0400

    Commit Inicial: Metodos de Java Reflection na Classe Cidade.

---
+++ src/main/java/ifam/model/Cidade.java
@@ -0,0 +1,33 @@
+package ifam.model;
+
+public class Cidade {
+
+    private String nome;
+    private String ibge;
+
+    private Estado estado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIbge() {
+        return ibge;
+    }
+
+    public void setIbge(String ibge) {
+        this.ibge = ibge;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/ifam/testes/TestandoJavaReflection.java
@@ -0,0 +1,99 @@
+package ifam.testes;
+
+import java.lang.reflect.*;
+
+public class TestandoJavaReflection {
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        String nomeDoPacote = "ifam.model";
+        String nomeDaClasse = "Cidade";
+
+        String nomeCompletoDaClasse = nomeDoPacote+"."+nomeDaClasse;
+
+        Class<?> classe = Class.forName(nomeCompletoDaClasse);
+
+        // Classe
+
+        System.out.println("Nome da Classe: "+classe.getCanonicalName());
+
+        int modificadoresDaClasse = classe.getModifiers();
+
+        System.out.println("Modificador da Classe:"+Modifier.toString(modificadoresDaClasse));
+
+        // Atributos
+
+        //Obtendo os atributos da classe
+
+        System.out.println("*** ATRIBUTOS ***");
+
+        Field[] atributos = classe.getDeclaredFields();
+
+        for(Field atributo: atributos){
+            // mostrando os dados de cada atributo
+            System.out.print(Modifier.toString(atributo.getModifiers())+" ");
+            System.out.print(atributo.getGenericType().getTypeName()+" ");
+            System.out.println(atributo.getName());
+        }
+
+        //Obtendo os metodos
+
+        System.out.println("*** METODOS ***");
+
+        Method[] metodos = classe.getDeclaredMethods();
+
+        for(Method metodo: metodos){
+            // mostrando os dados de cada atributo
+            System.out.print(Modifier.toString(metodo.getModifiers())+" ");
+            System.out.print(metodo.getReturnType().getTypeName()+" ");
+            System.out.print(metodo.getName());
+            System.out.print(" ( ");
+
+            Parameter[] parametros = metodo.getParameters();
+
+            for(Parameter parametro: parametros){
+                System.out.print(parametro.getType().getTypeName()+" ");
+                System.out.print(parametro.getName());
+            }
+
+            System.out.println(" ) ");
+
+            //Invocação de Métodos
+
+            // Método setNome
+            // public void setNome(String nome)
+
+            Class<?>[] parametrosSetNome = {String.class};
+
+            Method setNome = classe.getMethod("setNome",parametrosSetNome);
+
+            Object[] valoresSetNome = {"Manaus"};
+
+            //Criar um objeto da classe em tempo de execução
+
+            Object objetoCidade = classe.getDeclaredConstructor().newInstance();
+
+            setNome.invoke(objetoCidade,valoresSetNome);
+
+            // Método getNome
+            // public String getNome()
+
+            Class<?>[] parametrosGetNome = {};
+
+            Method getNome = classe.getMethod("getNome",parametrosGetNome);
+
+            Object[] valoresGetNome = {};
+
+            //Criar um objeto da classe em tempo de execução
+
+            //Usar o mesmo objeto cidade criado anteriormente para conseguir obter o valor da cidade.
+
+            Object retorno = getNome.invoke(objetoCidade,valoresGetNome);
+
+            System.out.println("Nome da Cidade:"+retorno);
+
+        }
+
+    }
+
+}
